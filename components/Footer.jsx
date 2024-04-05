cc3c229c4ca6b7da7a82001ffd22bbed46ed18cd
Harsh Chopra <chopraharshdeep@gmail.com>
2024-04-05 01:08:56 +0100

    test

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="flex flex-col items-center justify-between">
                 <Socials containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' iconStyles='text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all duration-200'/>
                 <div className="text-muted-foreground">
-                    Copyright &copy; Harsh Chopra. All rights reserved
+                    Copyright &copy; Harsh Chopra. All rights reserved init fam
                 </div>
             </div>
         </div>
