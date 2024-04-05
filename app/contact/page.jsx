89c3ecfffeb1afb00935b8b584de4e4adf1cf6a3
Harsh Chopra <chopraharshdeep@gmail.com>
2024-04-05 01:06:00 +0100

    First set of updates from template
    make changes to the about img and footer positioning for my projects bounce

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -15,26 +15,26 @@ const Contact = () => {
           <h1 className='h1 max-w-md mb-8'>lets work together</h1>
           <p className='subtitle max-w-[400px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur doloribus cumque voluptate quasi, blanditiis eum quod dolorem, et omnis delectus, dolorum mollitia quaerat voluptatum est minima ad reprehenderit ipsa laborum?</p>
         </div>
+        {/*illustration */}
+        <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
       </div>
-      {/*illustration */}
-      <div className='hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat'></div>
       <div className='grid xl:grid-cols-2 mb-24 xl:mb-32'>
        {/*info and form */}
        <div className='flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg'>
           {/*mail */}
           <div className='flex items-center gap-x-8'>
-            <MailIcon size={10} className='text-primary' />
+            <MailIcon size={20} className='text-primary' />
             <div>hcmedia@gmail.com</div>
           </div>
           {/*Address */}
           <div className='flex items-center gap-x-8'>
-            <HomeIcon size={10} className='text-primary' />
+            <HomeIcon size={20} className='text-primary' />
             <div>London, England</div>
           </div>
           {/*Number */}
             <div className='flex items-center gap-x-8'>
-            <PhoneCall size={10} className='text-primary' />
-            <div>+447803558147</div>
+            <PhoneCall size={20} className='text-primary' />
+            <div>(+44)7803 558147</div>
           </div>
         </div>
         <Form></Form>
